d815ea52671cbe59198207eba9aef8d7e97081d0
Pravin Sisowath <pravinsisowath@gmail.com>
2020-06-17 14:02:41 -0700

    components

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+        <h1 className="display-4">About Me</h1>
+        <p className="lead">This is the about me.</p>
+  </div>
+</div>
+    )
+}
+
+export default Jumbotron
---
+++ src/components/Media/Media.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Media = () => {
+    return (
+    <ul className="list-unstyled">
+    <li className="media">
+    <div className="media-body">
+      <h5 className="mt-0 mb-1">Project #1: eSPACE</h5>
+      Project description
+    </div>
+    </li>
+    <li className="media my-4"> 
+    <div className="media-body">
+      <h5 className="mt-0 mb-1">Project #2: notFacebook</h5>
+      Project description
+    </div>
+    </li>
+    <li className="media">
+    <div className="media-body">
+      <h5 className="mt-0 mb-1">Project #3</h5>
+      Project description
+    </div>
+  </li>
+</ul>
+    )
+}
+
+export default Media
