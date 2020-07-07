afe83bceedfcc9824ce6424dc7d9db3ee0a70572
Pravin Sisowath <pravinsisowath@gmail.com>
2020-07-07 00:44:18 -0700

    added more texts and links

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,10 @@ const Jumbotron = () => {
         <div className="jumbotron jumbotron-fluid">
         <div className="container">
         <h1 className="display-4">About Me</h1>
-        <p className="lead">This is the about me.</p>
+        <p className="lead">Hey! My name is Pravin Sisowath, and I'm currently a student attending the UCI Coding Bootcamp.</p>
+        <p>Here you can find the three main applications that were designed within the three month class period.</p>
+        <p>You can visit my github <a href='https://github.com/pravinsisowath'>by clicking here.</a></p>
+
   </div>
 </div>
     )
--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -6,19 +6,25 @@ const Media = () => {
     <li className="media">
     <div className="media-body">
       <h5 className="mt-0 mb-1">Project #1: eSPACE</h5>
-      Project description
+      A space education app dedicated to giving you the latest updates, news, and data about the universe.
+      <br></br>
+      <a href='https://liemnguyen0111.github.io/eSPACE/'>Check out eSPACE here.</a>
     </div>
     </li>
     <li className="media my-4"> 
     <div className="media-body">
       <h5 className="mt-0 mb-1">Project #2: notFacebook</h5>
-      Project description
+      A social media app that lets your fellow classmates post updates, comments, and images to stay in touch with each other.
+      <br></br>
+      <a href='https://n0tfacebook.herokuapp.com/'>Sign up for notFacebook here.</a>
     </div>
     </li>
     <li className="media">
     <div className="media-body">
-      <h5 className="mt-0 mb-1">Project #3</h5>
-      Project description
+      <h5 className="mt-0 mb-1">Project #3: ErrorFlow</h5>
+      An error forum that lets you post code that you're having trouble with, and lets people provide you with solutions.
+      <br></br>
+      Link coming soon!
     </div>
   </li>
 </ul>
